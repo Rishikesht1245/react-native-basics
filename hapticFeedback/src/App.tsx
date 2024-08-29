700ed1146dcb62c3348d1ef8054f9e564ecba113
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-08-30 00:52:57 +0530

    created dice roller

---
+++ hapticFeedback/src/App.tsx
@@ -0,0 +1,74 @@
+import { ImageSourcePropType, Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { PropsWithChildren, useState } from 'react'
+import { Image } from 'react-native'
+
+import DiceOne from "./assets/dice-1.png"
+import DiceTwo from "./assets/dice-2.png"
+import DiceThree from "./assets/dice-3.png"
+import DiceFour from "./assets/dice-4.png"
+import DiceFive from "./assets/dice-5.png"
+import DiceSix from "./assets/dice-6.png"
+
+
+// props passing to dice component (type declaration)
+type DiceProps = PropsWithChildren<{
+  // type provided by react native for images
+  imageUrl : ImageSourcePropType
+}>
+
+// dice component
+const Dice = ({imageUrl}:DiceProps) : JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl}/>
+    </View>
+  )
+}
+
+export default function App() {
+  // type for image 
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    switch(randomNumber){
+      case 1 : return setDiceImage(DiceOne);
+      case 2 : return setDiceImage(DiceTwo);
+      case 3 : return setDiceImage(DiceThree);
+      case 4 : return setDiceImage(DiceFour);
+      case 5 : return setDiceImage(DiceFive);
+      case 6 : return setDiceImage(DiceSix);
+    }
+  }
+  return (
+    <View style={styles.container}>
+        <Dice imageUrl={diceImage}/>
+        <Pressable onPress={rollDice}>
+          <Text style={styles.btn}>Roll Dice</Text>
+        </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container :{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"violet"
+  },
+
+  diceImage:{
+    width : 200,
+    height : 200,
+  },
+
+  btn:{
+    fontSize : 24,
+    fontWeight : 'bold',
+    marginTop : 20,
+    color : "white",
+    borderRadius : 8,
+    borderColor : "white"
+  }
+})
---
+++ hapticFeedback/src/index.d.ts
@@ -0,0 +1,3 @@
+// declaring module type for file import that do not have type declarations
+declare module "*.svg"
+declare module "*.png"
