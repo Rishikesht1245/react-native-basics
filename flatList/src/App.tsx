7db5844bc0df087b57e9e37a5fc91050ff2722f4
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-09-03 23:58:17 +0530

    implemented the currency converter function

--- flatList/src/App.tsx
+++ flatList/src/App.tsx
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,81 +10,53 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// constants
+import { currencies } from './constants';
+// Component
+import CurrencyButton from './components/CurrencyButton';
+// used for showing notifications
+import Snackbar from 'react-native-snackbar';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [inputValue, setInputValue] = useState<string>("");
+  const [resultValue, setResultValue] = useState<string>("");
+  const [targetCurrency, setTargetCurrency] = useState<string>("");
+
+  const buttonPressed = (targetValue : Currency) => {
+    if(!targetValue){
+      return Snackbar.show({
+        text : "Enter a value to convert",
+        backgroundColor : "#EA7773",
+        textColor : "#000000"
+      })
+    }
+
+    const inputAmount = parseFloat(inputValue);
+    if(!isNaN(inputAmount)){
+      const convertedValue = inputAmount * targetValue.value;
+      const result = `${targetValue.symbol} ${convertedValue.toFixed(2)} 😊`;
+
+      setResultValue(result);
+      setTargetCurrency(targetValue.name);
+    }else {
+      return Snackbar.show(
+        {
+          text : "Not a valid number to convert",
+          backgroundColor : "#EA7773",
+          textColor : "#000000"
+        }
+      )
+    }
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <StatusBar/>
+     <View>
+      <Text>App</Text>
+     </View>
     </SafeAreaView>
   );
 }
@@ -100,18 +65,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
